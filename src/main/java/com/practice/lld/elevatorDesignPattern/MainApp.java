97f4db22ef9cbfab35bdfee3c3fcb7dc410ad746
kumar gaurav <kumargaurav@kumars-MacBook-Air.local>
2025-12-26 18:02:33 +0530

    added elevator design
    Signed-off-by: kumar gaurav <kumargaurav@kumars-MacBook-Air.local>

---
+++ src/main/java/com/practice/lld/elevatorDesignPattern/MainApp.java
@@ -0,0 +1,8 @@
+package com.practice.lld.elevatorDesignPattern;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Starting main App...");
+    }
+}
